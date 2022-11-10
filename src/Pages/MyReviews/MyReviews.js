37fe95ed7c7d63ba97973291d40b80d74a21bb76
dev-nayandas <rich.nayan@gmail.com>
2022-11-10 15:26:59 +0600

    issue solved

--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,28 +1,47 @@
-import React, { useEffect, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { AuthContext } from '../../Contexts/AuthProvider/AuthProvider';
 import useTitle from '../../Hooks/UseTitle';
 import ReviewTable from './ReviewTable/ReviewTable';
 
 const MyReviews = () => {
     useTitle('My Reviews');
+    const {user} = useContext(AuthContext);
 
     const [myReviews, setMyReviews] = useState([])
     useEffect(()=>{
-      fetch('http://localhost:5000/reviews')
+      fetch(`http://localhost:5000/reviews/?email=${user.email}`)
       .then(res=>res.json())
       .then(data=>{
         setMyReviews(data)
         console.log(data)
       })
     },[])
+
+    const noReview = parseFloat(myReviews.length) === 0;
     return (
-        <div>
+        <div className='mb-5'>
             <h1 className='text-5xl mt-5 text-center mb-5 text-purple-800'>All of your reviews are here </h1>
+
+
             {
-                myReviews.map(review =><ReviewTable
-                key={review._id}
-                review={review}
-                ></ReviewTable>)
+              noReview?
+              <h1 className=' text-5xl mt-5 text-center mb-5 text-red-800' >No reviews are added</h1>
+              :
+
+              <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+              {
+                  myReviews.map(review =><ReviewTable
+                  key={review._id}
+                  review={review}
+                  ></ReviewTable>)
+              }
+              </div>
+
+
+              
             }
+            
+           
         </div>
     );
 };
--- src/Pages/MyReviews/ReviewTable/ReviewTable.js
+++ src/Pages/MyReviews/ReviewTable/ReviewTable.js
@@ -1,60 +1,43 @@
-import React from 'react';
+import { info } from 'daisyui/src/colors';
+import React, { useContext } from 'react';
+import { AuthContext } from '../../../Contexts/AuthProvider/AuthProvider';
 
 const ReviewTable = ({review}) => {
+  const {user} = useContext(AuthContext);
     console.log(review);
-    const {img, reviewText, reviewerName} = review;
+    const {_id,img, reviewText, reviewerName, serviceName} = review;
+    const handleDelete = _id => {
+        const procced = window.confirm('Are you sure you want to delete this review?');
+        if(procced){
+          fetch(`http://localhost:5000/reviews/${_id}`, {
+            method: 'DELETE',
+
+          })
+          .then(res=>res.json() )
+          .then(data =>{
+            console.log(data)
+          })
+
+          
+        }
+    };
+  
     return (
         <div>
-            <div>
-            <div className='text-brown-800 bg-base-100'>
-         
-         <div className="overflow-x-auto w-full">
-         <table className="table w-full">
-
-     <thead className='text-black'>
-  
-     </thead>
- <tbody>
-
-  
-
- 
-   
-   <tr className='text-black'>
-    
-     <td>
-       <div className="flex items-center space-x-3">
-         <div className="avatar">
-           <div className="mask mask-squircle w-12 h-12">
-             <img src={img} alt="Avatar Tailwind CSS Component" />
-           </div>
-         </div>
-         <div>
-           <div className="font-bold">{reviewerName}</div>
-           
-         </div>
-       </div>
-     </td>
-     <td>
-   
-       <span className="badge badge-ghost badge-sm">{reviewText}</span>
-     </td>
-   <td>
-   <button className="btn btn-secondary mr-2">Delete</button>
-<button className="btn btn-accent">Update</button>
-   </td>
-    
-   </tr>
-  
- 
- </tbody>
-
-
- 
-</table>
+            <div className="card w-96 bg-blue-300 shadow-xl mt-5">
+  <figure><img className='rounded-full mt-5' style={{width:'100px' ,height:'100px'}} src={img} alt="Shoes" /></figure>
+  <div className="card-body">
+    <h2 className="card-title">
+      Service Name : {serviceName}
+      
+    </h2>
+    <p>Review : {reviewText}</p>
+    <div className="card-actions justify-end">
+    <button onClick={() =>handleDelete(_id)} className="btn btn-secondary">Delete</button>
+    <button className="btn btn-accent">Update</button>
+    </div>
+  </div>
 </div>
-     </div>
-            </div>
         </div>
     );
 };
